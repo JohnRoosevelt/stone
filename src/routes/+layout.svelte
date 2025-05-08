160006f971a3ac3fd521d626561c99748d183201
john <john8.roosevelt@gmail.com>
2025-05-08 14:30:18 +0800

    adapter height

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -8,6 +8,7 @@
 
   // $inspect(DATAS).with(console.trace);
   const { children } = $props();
+  let innerHeight = $state(0)
 
   function handleVisibilityChange() {
     if (document.visibilityState === "visible") {
@@ -28,13 +29,12 @@
 <RouteLoading />
 <Dialog />
 
-<svelte:window bind:online={DATAS.online} />
+<svelte:window bind:innerHeight bind:online={DATAS.online} />
 <!-- <svelte:window bind:online={DATAS.online} /> -->
 <svelte:document onvisibilitychange={handleVisibilityChange} />
 
 <main
   w-screen
-  h-screen
   overflow-hidden
   flex-col
   flex-bc
@@ -42,6 +42,7 @@
   bg="#EDF1F0"
   text="black/85"
   dark="bg-[#111615] text-white"
+  style:height="{innerHeight}px"
 >
   {@render children()}
 </main>
