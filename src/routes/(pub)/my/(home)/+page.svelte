8b60d799e8cf42e778d71d9eb5cb4bd01529b2c0
john <john8.roosevelt@gmail.com>
2026-04-14 08:37:08 +0800

    add sqlite

--- src/routes/(pub)/my/(home)/+page.svelte
+++ src/routes/(pub)/my/(home)/+page.svelte
@@ -1,8 +1,5 @@
 <script>
   import { DATAS } from "$lib/data.svelte";
-  console.log(DATAS.uaInfo);
-
-  const ua = $derived(DATAS.uaInfo || {});
 </script>
 
 <svelte:head>
@@ -18,40 +15,40 @@
 
     <div>
       <span>device:</span>
-      <span text-xs font-mono>{ua.device?.type}</span>
-      <span text-xs font-mono>{ua.device?.vendor}</span>
-      <span text-xs font-mono>{ua.device?.model}</span>
+      <span text-xs font-mono>{DATAS.uaInfo?.device?.type}</span>
+      <span text-xs font-mono>{DATAS.uaInfo?.device?.vendor}</span>
+      <span text-xs font-mono>{DATAS.uaInfo?.device?.model}</span>
     </div>
 
     <div>
       <span>os:</span>
-      <span text-xs font-mono>{ua.os?.name}</span>
-      <span text-xs font-mono>{ua.os?.version}</span>
+      <span text-xs font-mono>{DATAS.uaInfo?.os?.name}</span>
+      <span text-xs font-mono>{DATAS.uaInfo?.os?.version}</span>
     </div>
 
     <div>
       <span>cpu:</span>
-      <span text-xs font-mono>{ua.cpu?.architecture}</span>
+      <span text-xs font-mono>{DATAS.uaInfo?.cpu?.architecture}</span>
     </div>
 
     <div>
       <span>browser:</span>
-      <span text-xs font-mono>{ua.browser?.name}</span>
-      <span text-xs font-mono>{ua.browser?.version}</span>
-      <span text-xs font-mono>{ua.browser?.major}</span>
-      <span text-xs font-mono>{ua.browser?.type}</span>
+      <span text-xs font-mono>{DATAS.uaInfo?.browser?.name}</span>
+      <span text-xs font-mono>{DATAS.uaInfo?.browser?.version}</span>
+      <span text-xs font-mono>{DATAS.uaInfo?.browser?.major}</span>
+      <span text-xs font-mono>{DATAS.uaInfo?.browser?.type}</span>
     </div>
 
     <div>
       <span>engine:</span>
-      <span text-xs font-mono>{ua.engine?.name}</span>
-      <span text-xs font-mono>{ua.engine?.version}</span>
+      <span text-xs font-mono>{DATAS.uaInfo?.engine?.name}</span>
+      <span text-xs font-mono>{DATAS.uaInfo?.engine?.version}</span>
     </div>
 
     <div>
       <span>sqlite version:</span>
-      <span text-xs font-mono>{DATAS.dbInfo.version}</span>
-      <span text-xs font-mono>OPFS: {DATAS.dbInfo.hasOPFS}</span>
+      <span text-xs font-mono>{DATAS.dbInfo?.version}</span>
+      <span text-xs font-mono>OPFS: {DATAS.dbInfo?.hasOPFS}</span>
     </div>
   </section>
 </article>
